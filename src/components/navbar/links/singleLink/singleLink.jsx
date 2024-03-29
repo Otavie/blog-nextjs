7e7bc2871ff1d869360b416acf813bfbe49e1244
Otavie <otavieokuoyo@gmail.com>
2024-03-29 16:35:20 +0100

    Created dir for single link

---
+++ src/components/navbar/links/singleLink/singleLink.jsx
@@ -0,0 +1,9 @@
+import styles from './singleLink.module.css'
+
+const SingleLink = (item) => {
+  return (
+    <div className={StyleSheet.container}>Single Link</div>
+  )
+}
+
+export default SingleLink
